58e94c0e8cc16490309a68e162575ac290c262b2
amacwan2428 <amacwan2428@conestogac.on.ca>
2022-07-29 14:30:15 -0400

    Initial Commit

---
+++ HealthQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthQuest
+//
+//  Created by user206114 on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
