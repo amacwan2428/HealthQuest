70b70cf77820835d88b78977d0846cd170953e51
Ruy Oliveira <ruygsgoliveira@live.com>
2022-08-03 17:47:21 -0400

    workout plan screen

---
+++ HealthQuest/PlanViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PlanViewController.swift
+//  HealthQuest
+//
+//  Created by Ruy Guilherme Silva Gomes de Oliveira on 2022-08-03.
+//
+
+import UIKit
+
+class PlanViewController: UIViewController {
+
+    @IBAction func SelectWorkout(_ sender: UIButton) {
+        print(sender.tag)
+ 
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
