303e6a6bb8ff1989a28ebc456f724dc2de4be45e
User <User@user.com>
2022-08-06 13:12:46 -0400

    Added Avatar and information in profile page

--- HealthQuest/ProfileViewController.swift
+++ HealthQuest/ProfileViewController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController{
 
+    @IBOutlet weak var imageAvatar: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        imageAvatar.setRounded()
     }
 
+}
+
+extension UIImageView {
+   func setRounded() {
+     layer.borderWidth = 1
+     layer.masksToBounds = false
+     layer.borderColor = UIColor.black.cgColor
+     layer.cornerRadius = self.frame.height / 2
+     clipsToBounds = true
+   }
 }
