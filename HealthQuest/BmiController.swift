1c3b4326d1b3c4ec9c3c93867237e80913349e6e
amacwan2428 <amacwan2428@conestogac.on.ca>
2022-08-07 20:36:16 -0400

    add gender and dob

--- HealthQuest/BmiController.swift
+++ HealthQuest/BmiController.swift
@@ -8,16 +8,56 @@
 import UIKit
 
 class BmiController: UIViewController {
+    let datePicker = UIDatePicker()
     @IBOutlet weak var weightTxt: UITextField!
     @IBOutlet weak var heightTxt: UITextField!
 
+    @IBOutlet weak var bdayText: UITextField!
+    @IBOutlet weak var rdiofemale: UIButton!
+    @IBOutlet weak var rdioMale: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createDatePickerView()
         // Do any additional setup after loading the view.
     }
         
-
+    func createDatePickerView(){
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(pressDone))
+        toolbar.setItems([doneButton], animated: true)
+        bdayText.inputAccessoryView = toolbar
+        bdayText.inputView = datePicker
+        datePicker.datePickerMode = .date
+    }
+    
+    @objc func pressDone(){
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        bdayText.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    @IBAction func rdioActionTwo(_ sender: UIButton) {
+        
+        if sender.tag == 1{
+            rdioMale.isSelected = true
+            rdiofemale.isSelected = false
+        } else if sender.tag == 2{
+            rdiofemale.isSelected = true
+            rdioMale.isSelected = false
+        }
+    }
+    @IBAction func rdioAction(_ sender: UIButton) {
+        
+        if sender.tag == 1 {
+            rdioMale.isSelected = true
+            rdiofemale.isSelected = false
+        } else if sender.tag == 2 {
+            rdioMale.isSelected = false
+            rdiofemale.isSelected = true
+        }
+    }
     @IBAction func onSubmitClick(_ sender: Any) {
         let w = Double(weightTxt.text!)
         let h = Double(heightTxt.text!)
